3d452d7c3926870fe3a7daf85d129c5baf29433f
JyotiSai <kajal.jojo@gmail.com>
2017-11-06 14:46:55 +0530

    Update HighestNumber.java

--- src/session2assignments/HighestNumber.java
+++ src/session2assignments/HighestNumber.java
@@ -11,16 +11,16 @@ public class HighestNumber
 	 */
 	public static void main(String[] args)
 	{
-		int a = 32, b = 11, c = 89; // Initialization of three variables
+		int firstNumber = 32, secondNumber = 11, thirdNumber = 89; // Initialization of three variables
 
-		if ((a > b) && (a > c)) // If condition statement to check whether a is the highest number 
+		if ((firstNumber > secondNumber) && (firstNumber > thirdNumber)) // If condition statement to check whether a is the highest number 
 		{
-			System.out.println("The highest number is: " + a); // Prints a as the highest number if above condition is true
+			System.out.println("The highest number is: " + firstNumber); // Prints a as the highest number if above condition is true
 			
 		}
 		else // else block if above condition is false
 		{
-			int highest = (b > c) ? b : c; // Ternary operator ?: is used to check the highest number between b and c
+			int highest = (secondNumber > thirdNumber) ? secondNumber : thirdNumber; // Ternary operator ?: is used to check the highest number between b and c
 			System.out.println("The highest number is: " + highest); // Prints the highest number
 		} // else block closed
 	} // main method closed
